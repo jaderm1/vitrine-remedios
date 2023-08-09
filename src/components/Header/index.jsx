18a6c5403fd4a652dec201c35b2722c3d2eeeea6
Jaderm1 <jadermaciel1@gmail.com>
2023-08-09 20:01:19 -0300

    criado a base do componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Header (){
+
+    
+    return (
+        
+        <header className='header'>
+            <img src="../src/img/logo.png" alt="" />
+            <h1 > PharmacyInHouse</h1>
+        </header>
+    )
+}
+export default Header
